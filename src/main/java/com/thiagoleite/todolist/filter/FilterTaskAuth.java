18f51ecc7ae5ea6ad501a152a68c3c5186430e59
Thiago Leite <thiago.0686@gmail.com>
2023-12-14 00:13:27 -0300

    feat: create filter

---
+++ src/main/java/com/thiagoleite/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,39 @@
+package com.thiagoleite.todolist.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecoded);
+
+        String[] credentials = authString.split(":");
+
+        String userName = credentials[0];
+
+        String password = credentials[1];
+
+
+        filterChain.doFilter(request, response);
+
+    }
+}
